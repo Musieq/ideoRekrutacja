b34c030bd765da02db7da73a6de29fd1c60661a3
Musieq <m.musielak97@gmail.com>
2022-01-17 15:42:50 +0100

    Page for displaying all categories

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'includes/header.php';
+?>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-5">
+            <h3>Kategorie</h3>
+
+            <!-- Tree with categories -->
+
+        </div>
+    </div>
+</div>
+
+
+<?php
+require_once 'includes/footer.php';
+?>
+
