ee6260e4220e0daf455f427329231f096b84b0fb
Musieq <m.musielak97@gmail.com>
2022-01-17 15:54:52 +0100

    Category class

---
+++ includes/class/category.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class Category {
+
+    private int $ID;
+    private string $name;
+    private int $parentID;
+
+
+    public function getAllCategories() {
+
+    }
+
+
+    public function displayCategoryTree() {
+
+    }
+
+
+    public function addCategory() {
+
+    }
+
+
+    public function validateData() {
+
+    }
+}
