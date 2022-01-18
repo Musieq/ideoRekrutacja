27974da78095c87e19fa3e25bee315fd1f7ce988
Musieq <m.musielak97@gmail.com>
2022-01-18 15:07:22 +0100

    Some navigation

--- includes/header.php
+++ includes/header.php
@@ -20,3 +20,21 @@ require_once 'includes/init.php';
     <title>Ideo rekrutacja</title>
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="category_add.php">Zarządzaj kategoriami</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="category_reorder.php">Zmień kolejność kategorii</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
