e4af24091798caa9879402f70636ea438d75f176
Musieq <m.musielak97@gmail.com>
2022-01-19 16:19:58 +0100

    Form for changing category order

--- category_reorder.php
+++ category_reorder.php
@@ -3,6 +3,11 @@ require_once 'includes/header.php';
 
 $category = new Category();
 
+if (isset($_POST['categoryReorder'])) {
+    $catOrder = $_POST['categoryOrder'];
+
+    $category->changeCategoryOrder($catOrder);
+}
 ?>
 
 
@@ -11,13 +16,16 @@ $category = new Category();
 
 
         <div class="col-lg-7">
-            <h3>Kategorie</h3>
+            <h3>Zmień kolejność kategorii (przenieś i upuść)</h3>
 
 
             <?php
             $category->displayCategoryList(false);
             ?>
-
+            <form action="category_reorder.php" method="post">
+                <input type="hidden" name="categoryOrder" id="categoryOrder">
+                <button type="submit" class="btn btn-primary" name="categoryReorder" id="categoryReorder">Submit</button>
+            </form>
 
         </div>
     </div>
