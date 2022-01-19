8667f3d0aa1f706612b2c03c6dbf6164ce31043f
Musieq <m.musielak97@gmail.com>
2022-01-19 16:19:10 +0100

    Script for sorting categories

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,9 @@
 <!-- Bootstrap Bundle with Popper -->
 <script src="js/bootstrap.min.js"></script>
 
+<!-- List sorting -->
+<script src="html5sortable-0.13.1/dist/html5sortable.js"></script>
+
 <!-- Main JS -->
 <script src="js/scripts.js"></script>
 
